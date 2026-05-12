fd906716c0121a4c09f6f0032375fc08a62d9f0b
gabrielgua <gabrieljmym13@gmail.com>
2026-05-12 15:12:14 -0300

    chore: create separated Icon component

---
+++ src/components/Icon.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+import { CircleHelp, type LucideIcon } from '@lucide/vue';
+
+withDefaults(defineProps<{
+  icon?: LucideIcon,
+  size?: number,
+}>(), {
+  icon: CircleHelp,
+  size: 18
+})
+</script>
+
+<template>
+  <component :is="icon" :size="size" v-bind="$attrs" />
+</template>
